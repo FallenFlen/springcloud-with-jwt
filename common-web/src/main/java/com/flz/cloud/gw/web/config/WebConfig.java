435088c05f72eaf9f76af2f51c7a4346d008e310
gtb-2022-gan-yunlong <faflzg430@aliyun.com>
2022-04-14 16:59:58 +0800

    change to use @ComponentScan

--- common-web/src/main/java/com/flz/cloud/gw/web/config/WebConfig.java
+++ common-web/src/main/java/com/flz/cloud/gw/web/config/WebConfig.java
@@ -2,11 +2,13 @@ package com.flz.cloud.gw.web.config;
 
 import com.flz.cloud.gw.web.interceptor.RequestInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final RequestInterceptor requestInterceptor;
