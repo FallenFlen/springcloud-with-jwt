db73366908e0169ae60e7d99b141d36ebe361dc9
gtb-2022-gan-yunlong <faflzg430@aliyun.com>
2022-04-15 16:01:50 +0800

    fix logic bug

--- common-web/src/main/java/com/flz/cloud/gw/web/interceptor/RequestInterceptor.java
+++ common-web/src/main/java/com/flz/cloud/gw/web/interceptor/RequestInterceptor.java
@@ -1,27 +1,42 @@
 package com.flz.cloud.gw.web.interceptor;
 
 import com.flz.cloud.gw.common.constants.Constant;
+import com.flz.cloud.gw.common.dto.ErrorResult;
 import com.flz.cloud.gw.common.dto.JwtUser;
+import com.flz.cloud.gw.common.utils.JsonUtils;
 import com.flz.cloud.gw.common.utils.JwtUtils;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class RequestInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String token = request.getHeader(Constant.TOKEN_KEY);
-        if (!StringUtils.isEmpty(token)) {
-            Long id = JwtUtils.getByKey(token, "id", Long.class);
-            String name = JwtUtils.getByKey(token, "name", String.class);
-            JwtUser jwtUser = JwtUtils.createJwtUser(id, name);
-            request.setAttribute("jwtUser", jwtUser);
+        if (!JwtUtils.validate(token)) {
+            responseError(response);
+            return false;
         }
+
+        Long id = JwtUtils.getByKey(token, "id", Long.class);
+        String name = JwtUtils.getByKey(token, "name", String.class);
+        JwtUser jwtUser = JwtUtils.createJwtUser(id, name);
+        request.setAttribute("jwtUser", jwtUser);
         return true;
+    }
+
+    private void responseError(HttpServletResponse response) {
+        response.setStatus(401);
+        response.addHeader("Content-Type", "application/json;charset=UTF-8");
+        try {
+            response.getWriter().write(JsonUtils.toJson(new ErrorResult("token无效")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
